21b582bb0cc42b2ce35355f408f8ae9f57ff018d
Darren Keenan <36712971+darrenkeenan@users.noreply.github.com>
2018-04-08 16:52:40 +0100

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,12 @@
+# Darren Keenan 2018-02-10
+# Exercise 3 - Collatz
+
+n = 17
+
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+        print(n)
+    elif n % 2 == 1:
+        n = n * 3 + 1
+        print(n) 
